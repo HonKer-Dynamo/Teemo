098b91c06fff062ab201fb043c9bcd878a03b2cf
HonKer-Dynamo <33470561+HonKer-Dynamo@users.noreply.github.com>
2018-09-07 00:43:39 +0800

    Add files via upload

---
+++ lib/domain2ip.py
@@ -0,0 +1,134 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'bit4'
+__github__ = 'https://github.com/bit4woo'
+
+import dns.resolver
+import dns.query
+from netaddr import IPAddress,IPNetwork
+import netaddr
+import sys,os
+import datetime
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from lib.common import strip_list
+
+def query(domain, record_type='A',server=None):
+    resolver = dns.resolver.Resolver()
+    if server != None:
+        resolver.nameservers = [server]
+    ips = []
+    try:
+        resp = resolver.query(domain, record_type, raise_on_no_answer=False)
+        if resp.response.answer:
+            tmpip = []
+            tmpcname = []
+            for i in resp.response.answer:
+                for j in i.items:
+                    try:
+                        #print "{0}   {1}".format(domain,j.address)
+                        tmpip.append(j.address)
+                    except:# CNAME here
+                        #print "{0}   {1}".format(domain,j.to_text())
+                        tmpcname.append(j.to_text())
+            line ="{0}\t{1}\t{2}".format(domain.ljust(30),", ".join(tmpcname),", ".join(tmpip))
+            if tmpip != None: #only collect IPs that don't use CDN （cname）
+                ips.extend(tmpip)
+            print line
+            return ips,line
+
+        # If we don't receive an answer from our current resolver let's
+        # assume we received information on nameservers we can use and
+        # perform the same query with those nameservers
+        if resp.response.additional and resp.response.authority:
+            ns = [
+                rdata.address
+                for additionals in resp.response.additional
+                for rdata in additionals.items
+            ]
+            resolver.nameservers = ns
+            return query(resolver, domain, record_type)
+        print domain
+        return [],domain
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoNameservers, dns.exception.Timeout):
+        print domain
+        return [],domain
+
+
+def domains2ips(domain_list,server=None):
+    IP_list =[]
+    lines = []
+    if domain_list.__len__()>0:
+        domain_list = strip_list(domain_list)
+        for domain in set(domain_list):
+            try:
+                ips,line = query(domain,record_type='A',server=server)
+                IP_list.extend(ips)
+                lines.append(line)
+            except Exception,e:
+                print e
+                #print domain
+        IP_list = list(set(IP_list))
+    return IP_list,lines
+
+def iprange(ip_str_list):
+    ip_str_list = list(set(ip_str_list))
+    ip_str_list= strip_list(ip_str_list)
+    #直接获取各个C段
+    subnet = set()
+    for ip_str in ip_str_list:
+        #print ip_str
+        ip = IPNetwork(ip_str)
+        ip.prefixlen =24
+        subnet.add(ip.cidr)
+    #print subnet
+
+    #尝试根据IP缩小范围
+    smaller_subnet =set()
+    for net in subnet:
+        tmpIPlist = []
+        for ip_str in ip_str_list:
+            ip = IPNetwork(ip_str)
+            ip.prefixlen = 24
+            if ip.cidr == net:#ip属于net
+                tmpIPlist.append(ip_str)
+        #print tmpIPlist
+        if len(tmpIPlist) == 1:
+            smaller_subnet.add(tmpIPlist[0])
+        elif len(tmpIPlist) >=2:
+            smaller = netaddr.spanning_cidr(tmpIPlist)  #type is IPNetwork
+            #if smaller != net:
+            smaller_subnet.add(smaller)
+        elif len(tmpIPlist) ==0:
+            print "{0} has no ip".format(net)
+
+    result = []
+    for item in smaller_subnet:
+        result.append(str(item))
+    return result
+
+def smaller_network():
+    #list = ['192.168.0.0', '192.168.0.245', '192.168.0.255']
+    #list = ['192.168.2.245']
+    x = netaddr.spanning_cidr(list)
+    print x
+
+def main(domainfile,outputfile=None):
+    if outputfile ==None:
+        now = datetime.datetime.now()
+        timestr = now.strftime("-%Y-%m-%d-%H-%M")
+        outputfile = "domain2ip" + timestr + ".txt"
+        outputfile= os.path.join(os.path.dirname(__file__), "..","output", outputfile)
+    if  os.path.exists(domainfile):
+        domains = open(domainfile,"r").readlines()
+        x, lines = domains2ips(domains)
+        lines.extend(x)
+        open(outputfile,"w").writelines("\n".join(lines))
+    else:
+        print("file not found")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print "{0} domain file".format(sys.argv[0])
+    else:
+        main(sys.argv[1])
---
+++ lib/zonetransfer.py
@@ -0,0 +1,101 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'bit4'
+__github__ = 'https://github.com/bit4woo'
+
+import os
+import re
+import dns.resolver, dns.zone
+from lib.log import logger
+
+class zonetransfer:
+    def __init__(self,domain):
+        self.domain= domain
+        self.nsservers = []
+        self.results =[]
+        self.has_zone_transfer = 0
+
+
+    def get_ns_server(self):
+        try:
+            resolver = dns.resolver.Resolver()
+            resolver.timeout = 5
+            resolver.lifetime = 10
+
+            # 使用阿里DNS服务器，效果不咋滴
+            #resolver.nameservers = ["223.5.5.5", "223.6.6.6"]
+
+            answers = resolver.query(self.domain, "NS")
+            if answers:
+                for answer in answers:
+                    #print answer
+                    self.nsservers.append(str(answer))
+        except Exception, e:
+            pass
+            #print "[-]get ns server error! try: dig %s NS +short" %(domain) , str(e)
+
+    def get_ns_server_nslookup(self):
+        #result = os.system("nslookup -type=ns {0}".format(domain))
+        result = os.popen('nslookup -type=ns ' + self.domain).read()
+        #print result
+        #其实域名后是否有点对axfr查询无影响
+        '''
+        if sys.platform == 'win32':
+            dns_servers = re.findall('nameserver = (.*?)\n', result)
+        else:
+            dns_servers = re.findall('nameserver = (.*?)\.\n', result)
+        '''
+        dns_servers = re.findall('nameserver = (.*?)\n', result)
+        self.nsservers.extend(dns_servers)
+
+    def axfr_check(self, domain, nsserver):
+        try:
+            zone = dns.zone.from_xfr(dns.query.xfr(str(nsserver), domain, timeout=5, lifetime=10))
+            if zone:
+                self.has_zone_transfer += 1
+                names = zone.nodes.keys()
+                names.sort()
+                for n in names:
+                    record = zone[n].to_text(n)
+                    self.results.append(record)
+                    print record
+        except Exception, e:
+            #print "[get xfr error]", domain, "\t", nsserver, str(e)
+            pass
+
+    def check(self):
+        logger.info("Doing Zone Transfer Check ...")
+        try:
+            self.get_ns_server()
+            self.get_ns_server_nslookup()
+        except Exception as e:
+            logger.error("Error in {0}: {1}".format(__file__.split('/')[-1],e))
+
+        if len(self.nsservers) == 0:
+            logger.info("None NS Server found for {0}.".format(self.domain))
+        else:
+            for _ in self.nsservers:
+                has_zone_transfer = self.axfr_check(self.domain, _)
+
+            if has_zone_transfer != 0 and len(self.results) != 0:
+                logger.info("Zone Transfer Detected for {0}".format(self.domain))
+                fp = open("..\\output\\{0}_zone_transfer.txt".format(self.domain), "wb")
+                fp.writelines(self.results)
+                fp.close()
+                for item in self.results:
+                    print item
+            if has_zone_transfer == 0 or len(self.results) == 0:
+                logger.info("Zone Transfer False")
+
+
+if __name__ == '__main__':
+    '''
+    a = get_ns_server("meizu.com")
+    b = get_ns_server_nslookup("meizu.com")
+    print a
+    print b
+
+    for _ in ["ns1.as6453.net"]:
+        z= axfr_check("bf",_)
+    '''
+    zonetransfer("insecuredns.com").check()
